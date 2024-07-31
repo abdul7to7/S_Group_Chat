d683761c8e62ac54e6c9edbbfbf6d8b6b47db16a
unknown <abdul7to7@gmail.com>
2024-07-31 21:23:58 +0530

    cors fix

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 // app.use(bodyParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+  })
+);
 
 const userRoutes = require("./routes/userRoutes");
 const sequelize = require("./util/db");
